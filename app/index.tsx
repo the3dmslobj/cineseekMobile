000043669813d73c9f851be1623ed0f8f53e6995
the3dmslobj <danielisthuta.3@gmail.com>
2025-08-09 03:27:17 +0700

    initial commit

---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="items-center flex-1 justify-center">
+      <Text className="text-5xl font-bold text-blue-300">Hola?</Text>
+    </View>
+  );
+}
